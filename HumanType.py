fbbbbc68818605f186f411941e12b76fd0b887e5
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2022-06-24 01:11:47 -0400

    Add files via upload

---
+++ HumanType.py
@@ -0,0 +1,11 @@
+import sys,time,random
+
+typing_speed = 50 #wpm
+def slow_type(t):
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/typing_speed)
+    print ('')
+
+slow_type("Replace this stand-in text wth anything you want")
